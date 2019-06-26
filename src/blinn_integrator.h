355e94365a9c11335e5e6c3f9ac2895a08066b5a
Eric Andrade <vloxflox@gmail.com>
2019-06-26 16:27:03 -0300

    indo pra casa

--- src/blinn_integrator.h
+++ src/blinn_integrator.h
@@ -12,5 +12,17 @@
 Blinn_integrator::Blinn_integrator(std::shared_ptr<Camera> camera):Sample_integrator(camera){}
 
 Color Blinn_integrator::Li( ray& r, Scene& scene){
-	
+	Color result_light(0,0,0);
+
+	if( scene.intersect(r,MINSIGHT,MAXSIGHT, surfaceInteraction) ){
+		
+		Material material = surfaceInteraction.primitive->material;
+
+		for(int i = 0;i < scene.lights.size(); i++ ){
+			if( scene.lights.at(i)->hit()  ){
+				ka = 
+
+			}
+		}
+	}
 }
